f5a01c1a29ad1f04b12eb4abf0963e8f33ca6d15
Yasuto1218 <94950313+Yasuto1218@users.noreply.github.com>
2022-12-05 08:35:14 +0900

    add :

---
+++ asset_manager/asset_management_simulate.py
@@ -0,0 +1,115 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import gaussian_kde 
+
+class AssetManagementSimulate():
+
+    def __init__(self, **kwargs):
+        self.interest_rate = kwargs["interest_rate"]
+        self.rate_risk = kwargs["rate_risk"]
+        self.operational_term_list = [5, 10, 20, 30]
+        self.invest_mumber_of_divesion = kwargs["invest_mumber_of_divesion"]
+        self.simulation_term = kwargs["simulation_term"]
+        self.simulate_size = 10000
+        self.total_investment_amount = kwargs["total_investment_amount"]
+        self.reserve_amount_a_year = self.total_investment_amount / self.invest_mumber_of_divesion #総投資額/分割回数(年)
+        self.target_profit_margin = kwargs["target_profit_margin"]
+        self.acceptable_risk = kwargs["acceptable_risk"]
+        self.x = np.linspace(-1, 6, 500)
+
+
+    #投資シミュレーションの結果を返す
+    def investment_result_cals(self):
+        result_list = np.array([])
+        result = np.array([])
+        for i in np.arange(1, self.simulation_term + 1): #for文1周で1年分10000サンプル
+            #平均が収益、標準偏差がリスクの正規分布を10000回サンプリングする
+            random_rate_list = np.random.normal(loc=self.interest_rate, scale=self.rate_risk, size=self.simulate_size) 
+            if i == 1 : #初年度　リストを作成
+                result = self.reserve_amount_a_year * random_rate_list
+                result_list = result
+            elif i <= self.invest_mumber_of_divesion: #投資期間なら (投資結果 + 積立額) * rate
+                result = (result + self.reserve_amount_a_year) * random_rate_list
+                result_list = np.vstack((result_list, result))
+            else:
+                result = result * random_rate_list
+                result_list = np.vstack((result_list, result))
+        return result_list
+
+
+    #分割投資の分布を結果をプロット
+    def _plot_installment_investment_return_distribution(self, result_list):
+        plt.figure(figsize=(10,7))
+        ax = plt.subplot()
+        per_list = []
+        loss_per_list = []
+        for term in self.operational_term_list:
+            how_many_times = result_list[term-1,:] / (self.total_investment_amount)
+            density_result = gaussian_kde(how_many_times) #密度推定
+            ax.plot(self.x, density_result(self.x), label=f'運用:{term}年')
+            percentage = len(how_many_times[how_many_times >= self.target_profit_margin]) / len(how_many_times) #目標とする運用に達する確率
+            per_list.append(percentage)
+            loss_per = len(how_many_times[how_many_times <= self.acceptable_risk]) / len(how_many_times) #元本割れする確率
+            loss_per_list.append(loss_per)
+          
+        plt.title(f'分割投資 : 年利{(self.interest_rate - 1) * 100:.1f}%, リスク{self.rate_risk * 100}%の商品利益確率', fontsize=18)
+        plt.legend(bbox_to_anchor=(0.95, 0.1), loc='lower right', borderaxespad=0, fontsize=16)
+        plt.savefig(f'output/分割投資 : 年利{(self.interest_rate - 1) * 100:.1f}%, リスク{self.rate_risk * 100}%の商品利益確率.jpeg')
+        plt.show()
+        
+        for i in [f'{term}年後に資産が{self.target_profit_margin:,}倍以上になる確率{per * 100:.1f}%' for term, per in zip(self.operational_term_list, per_list)]:
+            print(i)
+        print('\n')
+        for i in [f'{term}年後に資産が{np.round((1 - self.acceptable_risk) * 100)}%以上減少している確率{per * 100:.1f}%' for term, per in zip(self.operational_term_list, loss_per_list)]:
+            print(i)
+
+
+    #シミュレーション結果を受けて分割投資の分布をプロット
+    def plot_installment_investment_distribution(self):
+        result_list = self.investment_result_cals()
+        self._plot_installment_investment_return_distribution(result_list)
+
+
+    def average_profit_list(self):
+        result_list = self.investment_result_cals()
+        result_mean = result_list.mean(axis=1)
+        x = np.arange(1, self.simulation_term+1)
+        invest_list = [self.reserve_amount_a_year * i if i <= self.invest_mumber_of_divesion else self.total_investment_amount for i in x]
+        average_profit_list = result_mean - np.array(invest_list)
+        return average_profit_list
+
+    
+    def median_profit_list(self):
+        result_list = self.investment_result_cals()
+        result_median = np.median(result_list, axis=1)
+        x = np.arange(1, self.simulation_term+1)
+        invest_list = [self.reserve_amount_a_year * i if i <= self.invest_mumber_of_divesion else self.total_investment_amount for i in x]
+        median_profit_list = result_median - np.array(invest_list)
+        return median_profit_list
+
+
+    def simulate_plot(self):
+        result_list = self.investment_result_cals()
+        result_mean = result_list.mean(axis=1)
+        result_median = np.median(result_list, axis=1)
+        result_std = result_list.std(axis=1)
+        x = np.arange(1, self.simulation_term+1)
+        
+        result_upper = result_std * 1.64 + result_mean
+        result_lower = result_std * -1.64 + result_mean
+        fig = plt.figure(figsize=(12, 7))
+        ax1 = fig.add_subplot(1, 1, 1)
+        ax1.plot(x, result_mean, label='平均') 
+        ax1.plot(x, result_median,c="b", label="中央値", alpha=0.7) #中央値
+        ax1.plot(x, [self.reserve_amount_a_year * i if i <= self.invest_mumber_of_divesion else self.total_investment_amount for i in x], label="投資元本", alpha=0.7) #単純投資合計額
+
+        # ax1.fill_between(x, result_upper, result_lower, color='g', alpha=0.1, label='90信頼区間') #信頼区間
+
+        plt.ticklabel_format(style='plain',axis='y')
+        plt.title(f'分割投資 : 年利{(self.interest_rate - 1)* 100:.1f}%, リスク{self.rate_risk * 100}%、{self.invest_mumber_of_divesion}年で投資額{self.total_investment_amount:,}円', fontsize=18)
+        plt.grid()
+        # plt.yticks((np.round(result_lower), np.round(result_upper), 2000000))
+        plt.legend(bbox_to_anchor=(0.25, 0.95), loc='upper right', borderaxespad=0, fontsize=16)
+        plt.savefig(f'output/年利{(self.interest_rate - 1)* 100:.1f}%, リスク{self.rate_risk * 100}%、{self.invest_mumber_of_divesion}年で投資額{self.total_investment_amount:,}円.jpeg')
+        plt.show()
+
---
+++ life_plan_manager/life_plan_simulate.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class LifePlanSimulate():
+    def __init__(self, **kwargs):
+        self.pension_amount = kwargs["pension_amount"]
+        self.cost_of_living = kwargs["cost_of_living"]
+        self.severance_pay = kwargs["severance_pay"]
+        self.current_saving = kwargs["current_saving"]
+        self.age = 60 #定年
+        self.simulate_age = kwargs["simulate_age"]
+        self.x_ticks = np.linspace(-1, 6, 500)
+        #車関係
+        self.car_purchase_frequency = kwargs["car_purchase_frequency"]
+        self.car_price   = kwargs["car_price"]
+        #住宅
+        self.house_fix_price_a_year = 5000000 / (self.simulate_age - self.age)
+
+
+    def plot_life_plan_simulate_without_asset_management(self):
+        all_property, age_list, all_life_simulate_list = self.life_plan_calc()
+        self.plot_figure_of_life_plan(all_property, age_list, all_life_simulate_list)
+
+
+    def life_plan_calc(self):
+        all_property = self.current_saving + self.severance_pay #総資産
+        all_property_list = [] #年ごとの残高の確認
+        age_list = np.arange(self.age, self.simulate_age + 1)
+        
+        for age in age_list:
+            if age <= 64: #年金支給前
+                all_property_list.append(all_property)
+                all_property -= self.cost_of_living #生活費がかかるだけ
+            else: #年金支給後:
+                all_property -= self.cost_of_living  
+                all_property += self.pension_amount #年金の支給開始
+                all_property_list.append(all_property)
+
+        car_simulation_list, house_fix_simulation_list = self._add_car_house_simulation()
+        car_simulation_list = np.array(car_simulation_list)
+        house_fix_simulation_list = np.array(house_fix_simulation_list)
+        all_property_list = np.array(all_property_list)
+        all_life_simulate_list = np.subtract(all_property_list, car_simulation_list)
+        all_life_simulate_list = np.subtract(all_life_simulate_list, house_fix_simulation_list)
+        return all_property, age_list, all_life_simulate_list
+
+
+    def _add_car_house_simulation(self):
+        #車の購入
+        car_simulation_list = [] 
+        age_list = np.arange(self.age, self.simulate_age + 1)
+        for age in age_list:
+            if age %  self.car_purchase_frequency == 0: #年金支給前
+                car_simulation_list.append(self.car_price)
+            else:
+                car_simulation_list.append(0)
+        #家の修理
+        house_fix_price_a_year = 5000000 / (self.simulate_age + 1 - self.age)
+        house_fix_simulation_list = [house_fix_price_a_year] * (self.simulate_age + 1 - self.age)
+        return car_simulation_list, house_fix_simulation_list    
+
+
+    def plot_figure_of_life_plan(self, all_property, age_list, all_life_simulate_list):
+        fig = plt.figure(figsize=(10, 7))
+        ax1 = fig.add_subplot(1, 1, 1)
+        ax1.bar(age_list, all_life_simulate_list, alpha=0.3, color='red')
+        plt.ticklabel_format(style='plain',axis='y')
+        plt.title(f'資産額:{all_property:,}円({self.simulate_age}歳時点)', fontsize=14)
+        plt.grid()
+        plt.yticks(np.arange(0, all_life_simulate_list[0] * 1.1, 2000000))
+        plt.show()
+
+
+    def plot_figure_of_life_plan_in_invest(self, age_list, all_life_simulate_list, invest_life_plan_simulate_list):
+        fig = plt.figure(figsize=(10, 7))
+        ax1 = fig.add_subplot(1, 1, 1)
+        ax1.bar(age_list, invest_life_plan_simulate_list, alpha=0.9, color='blue')
+        ax1.bar(age_list, all_life_simulate_list, alpha=0.9, color='red')
+        plt.ticklabel_format(style='plain',axis='y')
+        plt.title(f'投資ありのライフプラン({self.age}から{self.simulate_age}歳)', fontsize=14)
+        plt.grid()
+        plt.yticks(np.arange(0, invest_life_plan_simulate_list[-1] * 1.1, 5000000))
+        plt.show()
